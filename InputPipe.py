6ce723353b403ff05d4c7bd5339fca2a9c9caee7
Sezoir <joshmottley0@gmail.com>
2020-07-06 18:32:25 +0100

    - Added function to count number of images that will be stored. - In the loadAllImages function:    * Now stores images in a numpy array.    * Fixed bug when making margins slightly bigger.    * Changed resize to resize_with_pad to preserve aspect ratio.

--- InputPipe.py
+++ InputPipe.py
@@ -26,7 +26,27 @@ class InputPipe():
         # Return image as a numpy array
         return np.array(img)
 
+    def getImgCount(self):
+        counter = 0
+        # Go through each breed folder
+        for breedFolder in listdir(self.mAnnPath):
+            # Go through each file in the breed folder
+            for file in listdir(self.mAnnPath / breedFolder):
+                # Create path to file
+                filePath = Path(breedFolder, file)
+                # Read the image and the annotations tree
+                objects = ET.parse(self.mAnnPath / filePath).getroot().findall('object')
+                for object in objects:
+                    counter += 1
+        return counter
+
     def loadAllImages(self):
+        # Get count of images
+        count = self.getImgCount()
+        # Initialise empty numpy array
+        self.mImages = np.zeros((count, self.mImageWidth, self.mImageHeight, self.mImageChannels))
+        # Create index
+        index = 0
         # Go through each breed folder
         for breedFolder in listdir(self.mAnnPath):
             # Go through each file in the breed folder
@@ -54,7 +74,7 @@ class InputPipe():
                     # Make the margins slightly bigger
                     xMin = max(0, xMin - 4)
                     xMax = min(width, xMax + 4)
-                    yMin = max(0, xMin - 4)
+                    yMin = max(0, yMin - 4)
                     yMax = min(height, yMax + 4)
                     # Crop the image to focus on the specific dog
                     # @todo: test whether adding a wider margin helps the discriminator more
@@ -69,13 +89,17 @@ class InputPipe():
                     else:
                         interpolation = tf.image.ResizeMethod.BICUBIC   # Stretch
 
-                    # Resize image
-                    imgCropped = tf.image.resize(imgCropped, (self.mImageHeight, self.mImageWidth),
-                                            method=interpolation, antialias=True)
+                    # Resize image with pad to reserve aspect ratio
+                    imgCropped = tf.image.resize_with_pad(imgCropped, self.mImageHeight, self.mImageWidth,
+                                                          method=interpolation, antialias=True)
 
+                    # Store image
+                    self.mImages[index] = imgCropped
+                    # Increase index
+                    index += 1
 
-                    plt.imshow(imgCropped)
-                    plt.show()
+                    # plt.imshow(imgCropped)
+                    # plt.show()
 
 
 
@@ -87,12 +111,17 @@ class InputPipe():
     mImageChannels = 3
     mAnnPath = Path("Datasets\\annotations\\Annotation")
     mImPath = Path("Datasets\\images\\Images")
+    mImages = None
 
 
 
 if __name__ == "__main__":
     io = InputPipe()
-    imga = io.readImage("Datasets\\images\\Images\\n02085620-Chihuahua\\n02085620_199.jpg")
+    # imga = io.readImage("Datasets\\images\\Images\\n02085620-Chihuahua\\n02085620_199.jpg")
     io.loadAllImages()
-    # plt.imshow(imga, interpolation='nearest')
-    # plt.show()
+    imga = io.mImages[0]
+    plt.imshow(imga, interpolation='nearest')
+    plt.show()
+    imga = io.mImages[1]
+    plt.imshow(imga, interpolation='nearest')
+    plt.show()
