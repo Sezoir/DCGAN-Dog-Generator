bf1336638f5c5541b2da425743102f66c40f72b8
Sezoir <joshmottley0@gmail.com>
2020-07-13 16:38:24 +0100

    - Added more parameters to control the creation of the model during initialisation of the class. - Added parameter names when calling functions in creating the model for clearer reading. - Model will now default draw initial weights from a random uniform distribution. - Removed creation of initial weights in the load function (now depends on parameter passed in on creation of class). - Model first layer output space now depends on the image the product of the image height and width that is controlled by a "scale" variable. - tconvReLU function now use initial weights.

--- Generator.py
+++ Generator.py
@@ -3,8 +3,18 @@ import tensorflow.keras as ks
 import tensorflow.keras.layers as lr
 
 class Generator:
-    def __init__(self, batchSize: int = 32):
+    def __init__(self, batchSize: int = 32,
+                 imShape: (int, int, int) = (64, 64, 3),
+                 scale: float = 0.5,
+                 load: bool = False,
+                 initWeights: ks.initializers.Initializer = ks.initializers.RandomUniform(-1, 1)):
         self.mBatchSize = batchSize
+        self.mImHeight = imShape[0]
+        self.mImWidth = imShape[1]
+        self.mImChannels = imShape[2]
+        self.mScale = int(4 ** 1/scale)
+        self.mLoad = load
+        self.mInitWeights = initWeights
         return
 
     def load(self):
@@ -13,28 +23,28 @@ class Generator:
 
             return
         # Else we create a new initial model
-        initWeights = ks.initializers.TruncatedNormal(stddev=0.02, mean=0)
-        self.mModel = self.createModel(initWeights)
+        self.mModel = self.createModel()
         return
 
-    def createModel(self, initWeights):
+    def createModel(self):
         model = ks.Sequential()
-        model.add(lr.Dense(8*8*512, use_bias=False, input_shape=(100,)))
+        model.add(lr.Dense((self.mImHeight//self.mScale)*(self.mImWidth//self.mScale)*512,
+                           use_bias=False, input_shape=(100,), kernel_initializer=self.mInitWeights))
         model.add(lr.BatchNormalization())
         model.add(lr.LeakyReLU())
-        model.add(lr.Reshape((8, 8, 512)))
-        model = self.tconvReLU(model, 256, (5, 5), 1)
-        model = self.tconvReLU(model, 128, (5, 5), 2)
-        model = self.tconvReLU(model, 64, (5, 5), 2)
-        model = self.tconvReLU(model, 32, (5, 5), 2)
-        model.add(lr.Dense(3, activation="tanh", kernel_initializer=initWeights))
-        # model = self.tconvReLU(model, 1, (5, 5), 2)
+        model.add(lr.Reshape((self.mImHeight//self.mScale, self.mImWidth//self.mScale, 512)))
+        model = self.tconvReLU(model, output=256, shape=(5, 5), stride=1)
+        model = self.tconvReLU(model, output=128, shape=(5, 5), stride=2)
+        model = self.tconvReLU(model, output=64, shape=(5, 5), stride=2)
+        model = self.tconvReLU(model, output=32, shape=(5, 5), stride=2)
+        model.add(lr.Dense(3, activation="tanh", kernel_initializer=self.mInitWeights))
         return model
 
     def tconvReLU(self, model: ks.Sequential, output: int, shape: (int, int),
-                  stride: int, padding="same", useBias=False,
+                  stride: int, padding: str = "same", useBias: bool = False,
                   slope: float = 0.2) -> ks.Sequential:
-        model.add(lr.Conv2DTranspose(output, shape, strides=(stride, stride), padding=padding, use_bias=useBias))
+        model.add(lr.Conv2DTranspose(output, shape, strides=(stride, stride), padding=padding, use_bias=useBias,
+                                     kernel_initializer=self.mInitWeights))
         model.add(lr.BatchNormalization())
         model.add(lr.LeakyReLU(alpha=slope))
         return model
@@ -57,19 +67,26 @@ class Generator:
 
     mBatchSize = None
     mLoad = False
+    mImHeight = None
+    mImWidth = None
+    mImChannels = None
+    mScale = None
+    mInitWeights = None
     mModel = None
 
 if __name__ == "__main__":
     from InputPipe import InputPipe
-    gen = Generator()
+    gen = Generator(imShape=(64, 64, 3),
+                    load=False,
+                    initWeights=ks.initializers.TruncatedNormal(stddev=0.02, mean=0))
     gen.load()
     mod = gen.mModel
     print(mod.summary())
     print(mod.output_shape)
-    import matplotlib.pyplot as plt
-    noise = tf.random.normal([1,100])
-    generatedImage = mod(noise, training=False)
-    plt.imshow(generatedImage[0,:,:,0], cmap='gray')
-    plt.show()
-    plt.imshow(generatedImage[0, :, :, 1], cmap='gray')
-    plt.show()
+    # import matplotlib.pyplot as plt
+    # noise = tf.random.normal([1,100])
+    # generatedImage = mod(noise, training=False)
+    # plt.imshow(generatedImage[0,:,:,0], cmap='gray')
+    # plt.show()
+    # plt.imshow(generatedImage[0, :, :, 1], cmap='gray')
+    # plt.show()
