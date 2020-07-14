304961c399a45d9d873ec3e8168c5ba102e36199
Sezoir <joshmottley0@gmail.com>
2020-07-14 20:12:01 +0100

    - Added training step, training and picture generation functions. - Batchsize is now determined by DCGAN variable.

--- DCGAN.py
+++ DCGAN.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+import time
 
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as ks
+import matplotlib.pyplot as plt
 
 from InputPipe import InputPipe
 from Discriminator import Discriminator
@@ -10,12 +12,12 @@ from Generator import Generator
 
 class DCGAN:
     def __init__(self):
-        self.mInputPipe = InputPipe()
-        self.mInputPipe.loadAllImages(sampleSize=100)
+        self.mInputPipe = InputPipe(batchSize=self.mBatchSize)
+        self.mInputPipe.loadAllImages()#sampleSize=100
         initWeights = ks.initializers.TruncatedNormal(stddev=0.02, mean=0)
-        self.mDiscriminator = Discriminator(self.mInputPipe.mImages, batchSize=self.mInputPipe.mBatchSize,
+        self.mDiscriminator = Discriminator(batchSize=self.mBatchSize,
                                             imShape=self.mInputPipe.mShape, initWeights=initWeights)
-        self.mGenerator = Generator(batchSize=self.mInputPipe.mBatchSize, imShape=self.mInputPipe.mShape,
+        self.mGenerator = Generator(batchSize=self.mBatchSize, imShape=self.mInputPipe.mShape,
                                     initWeights=initWeights)
         return
 
@@ -31,13 +33,50 @@ class DCGAN:
 
     # The training step for both models for 1 epoch
     @tf.function
-    def trainStep(self):
+    def trainStep(self, images):
+        noise = tf.random.normal([self.mBatchSize, self.mNoiseDim])
 
+        with tf.GradientTape() as genTape, tf.GradientTape() as discTape:
+            generatedImages = self.mGenerator.mModel(noise, training=True)
+
+            realOutput = self.mDiscriminator.mModel(images, training=True)
+            fakeOutput = self.mDiscriminator.mModel(generatedImages, training=True)
+
+            genLoss = self.mGenerator.loss(realOutput, fakeOutput)
+            discLoss = self.mDiscriminator.loss(realOutput, fakeOutput)
+
+        gradGen = genTape.gradient(genLoss, self.mGenerator.mModel.trainable_variables)
+        gradDisc = discTape.gradient(discLoss, self.mDiscriminator.mModel.trainable_variables)
+
+        self.mGenerator.mOptimizer.apply_gradients(zip(gradGen, self.mGenerator.mModel.trainable_variables))
+        self.mDiscriminator.mOptimizer.apply_gradients(zip(gradDisc, self.mDiscriminator.mModel.trainable_variables))
         return
 
     # Trains both models for x epochs
-    def train(self):
+    def train(self, epochs: int):
+        for epoch in range(epochs):
+            start = time.time()
 
+            for imageBatch in self.mInputPipe.mImages:
+                self.trainStep(imageBatch)
+
+            if (epoch+1) % 15 == 0:
+                self.save()
+
+            print('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+        self.genPic()
+        return
+
+    def genPic(self):
+        # Get random seed
+        seed = tf.random.normal([1, self.mNoiseDim])
+        # Generate image with seed
+        prediction = self.mGenerator.mModel(seed, training=False)
+        # Unstandardisation the image
+        prediction = (prediction+1)*127.5
+        # Show image
+        plt.imshow(prediction.numpy()[0])
+        plt.show()
         return
 
 
@@ -47,12 +86,14 @@ class DCGAN:
     mDiscriminator = None
     mGenerator = None
     mSaveDir = Path("TrainingCheckpoints/")
-    mCheckPoint = None
+    mBatchSize = 32
+    mNoiseDim = 100 # Size input to generator
 
 if __name__ == "__main__":
     gan = DCGAN()
     gan.loadModels(loadCheckpoint="initial")
-    print(gan.mDiscriminator.mModel.weights)
-    gan.save()
-    gan.loadModels(loadCheckpoint="latest")
-    print(gan.mDiscriminator.mModel.weights)
+    # print(gan.mDiscriminator.mModel.weights)
+    # gan.save()
+    # gan.loadModels(loadCheckpoint="latest")
+    # print(gan.mDiscriminator.mModel.weights)
+    gan.train(50)
