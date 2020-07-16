abf0229a2c3b706e5b53d06b64a8d3edeed14ed1
Sezoir <joshmottley0@gmail.com>
2020-07-16 21:10:05 +0100

    - Shape of image is now determined by DCGAN. - train function now record the loss of each model per epoch. - Added new function: plotLoss, which displays a line graph of the loss of each model as the model trains. - Changed genPic function to be able to generate and display multiple samples from the current Generator model (as opposed to just one).

--- DCGAN.py
+++ DCGAN.py
@@ -1,10 +1,14 @@
 from pathlib import Path
-import time
+from math import isqrt
 
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as ks
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+sns.set(style="darkgrid")
+
 from tqdm import tqdm
 
 from InputPipe import InputPipe
@@ -13,11 +17,12 @@ from Generator import Generator
 
 class DCGAN:
     def __init__(self, sampleSize=None):
-        self.mInputPipe = InputPipe(batchSize=self.mBatchSize)
+        self.mInputPipe = InputPipe(batchSize=self.mBatchSize, imageWidth=self.mImageShape[0],
+                                    imageHeight=self.mImageShape[1], imageChannels=self.mImageShape[2])
         self.mInputPipe.loadAllImages(sampleSize=sampleSize)
         initWeights = ks.initializers.TruncatedNormal(stddev=0.02, mean=0)
         self.mDiscriminator = Discriminator(batchSize=self.mBatchSize,
-                                            imShape=self.mInputPipe.mShape, initWeights=initWeights)
+                                            imShape=self.mImageShape, initWeights=initWeights)
         self.mGenerator = Generator(batchSize=self.mBatchSize, imShape=self.mInputPipe.mShape,
                                     initWeights=initWeights)
         return
@@ -43,65 +48,88 @@ class DCGAN:
             realOutput = self.mDiscriminator.mModel(images, training=True)
             fakeOutput = self.mDiscriminator.mModel(generatedImages, training=True)
 
-            genLoss = self.mGenerator.loss(realOutput, fakeOutput)
-            discLoss = self.mDiscriminator.loss(realOutput, fakeOutput)
+            genLoss = self.mGenerator.loss(realOutput, fakeOutput, lossFunc=self.mLossFunc)
+            discLoss = self.mDiscriminator.loss(realOutput, fakeOutput, lossFunc=self.mLossFunc)
 
-        gradGen = genTape.gradient(genLoss, self.mGenerator.mModel.trainable_variables)
         gradDisc = discTape.gradient(discLoss, self.mDiscriminator.mModel.trainable_variables)
+        gradGen = genTape.gradient(genLoss, self.mGenerator.mModel.trainable_variables)
 
-        self.mGenerator.mOptimizer.apply_gradients(zip(gradGen, self.mGenerator.mModel.trainable_variables))
         self.mDiscriminator.mOptimizer.apply_gradients(zip(gradDisc, self.mDiscriminator.mModel.trainable_variables))
+        self.mGenerator.mOptimizer.apply_gradients(zip(gradGen, self.mGenerator.mModel.trainable_variables))
         return (genLoss, discLoss)
 
     # Trains both models for x epochs
     def train(self, epochs: int):
+        # Initialise loss arrays
+        self.mDiscLoss = np.zeros(epochs)
+        self.mGenLoss = np.zeros(epochs)
+        # Loop through each epoch
         for epoch in range(epochs):
-            # start = time.time()
-
             with tqdm(total=tf.data.experimental.cardinality(self.mInputPipe.mImages).numpy()) as pbar:
                 for imageBatch in self.mInputPipe.mImages:
                     (genLoss, discLoss) = self.trainStep(imageBatch)
                     pbar.set_description("Progress for epoch {%s}" % epoch)
                     pbar.set_postfix_str("Generator loss: {:.5f}, Discriminator loss: {:.5f}".format(genLoss, discLoss))
-                    # pbar.set_description("Generator loss: %s, Discriminator loss: %s, Progress for epoch {%s}" % genLoss, discLoss, epoch)
                     pbar.update(1)
+                    self.mDiscLoss[epoch] = discLoss
+                    self.mGenLoss[epoch] = genLoss
                 pbar.close()
 
             if (epoch+1) % 15 == 0:
                 self.save()
         return
 
-    def genPic(self):
+    def genPic(self, sample=1):
         # Get random seed
-        seed = tf.random.normal([1, self.mNoiseDim])
+        seed = tf.random.normal([sample, self.mNoiseDim])
         # Generate image with seed
         prediction = self.mGenerator.mModel(seed, training=False)
         # Unstandardisation the image
-        prediction = (prediction.numpy()[0]+1)/2
-        # Show image
-        plt.imshow(prediction)
+        prediction = (prediction.numpy()+1)/2
+        # Calculate figure size
+        nextSquare = isqrt(sample)+1
+        # Get figure
+        fig = plt.figure(figsize=(8, 8))
+        # Add subplots
+        for ind in range(1, sample+1):
+            ax = fig.add_subplot(nextSquare, nextSquare, ind)
+            ax.set_axis_off()
+            plt.imshow(prediction[ind-1])
         plt.show()
         return
 
-
-
+    def plotLoss(self):
+        df = pd.DataFrame({
+            "Epoch": np.arange(len(self.mDiscLoss)),
+            "discLoss": self.mDiscLoss,
+            "genLoss": self.mGenLoss
+        })
+        sns.lineplot(x='Epoch', y='value', hue='variable', data=pd.melt(df, ['Epoch']))
+        plt.show()
+        return
 
     mInputPipe = None
     mDiscriminator = None
     mGenerator = None
     mSaveDir = Path("TrainingCheckpoints/")
-    mBatchSize = 64
+    mBatchSize = 48
     mNoiseDim = 100 # Size input to generator
+    mGenLoss = None
+    mDiscLoss = None
+    mImageShape = (64, 64, 3)
+    mLossFunc = "ralsgan"
 
 if __name__ == "__main__":
-    gan = DCGAN(100)
-    gan.loadModels(loadCheckpoint="latest")
-    gan.genPic()
+    gan = DCGAN()
+    gan.loadModels(loadCheckpoint="initial")
+    gan.train(10)
+    gan.plotLoss()
+    gan.genPic(sample=15)
     # print(gan.mDiscriminator.mModel.weights)
-    # gan.save()
+    gan.save()
     # gan.loadModels(loadCheckpoint="latest")
     # print(gan.mDiscriminator.mModel.weights)
-    gan.train(10)
-    gan.genPic()
-    gan.save()
+    # gan.train(20)
+    # gan.genPic()
+    # gan.save()
 
