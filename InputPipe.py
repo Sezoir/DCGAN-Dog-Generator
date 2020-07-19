42cc6e177c129ea1eec55285b8189b42012e82e1
Sezoir <joshmottley0@gmail.com>
2020-07-19 23:57:28 +0100

    - Added a generator function to generate "chunks" of the images (as opposed to loading all images into memory).

--- InputPipe.py
+++ InputPipe.py
@@ -1,4 +1,5 @@
 from PIL import Image
+
 import numpy as np
 import xml.etree.ElementTree as ET
 from pathlib import Path
@@ -118,6 +119,88 @@ class InputPipe():
                         return
         return
 
+    # @todo: Move areas of function to its own function
+    # @todo: Scale group depending on sampleSize
+    def getNextImageChunk(self, sampleSize=None, group=3):
+        # Get count of images
+        if sampleSize == None:
+            sampleSize = self.getImgCount()
+
+        fileFolder = {folder: [file for file in listdir(self.mAnnPath / folder)] for folder in listdir(self.mAnnPath)}
+        groupSize = {}
+        # Determine sample size per folder
+        for breedFolder in listdir(self.mAnnPath):
+            groupSize[breedFolder] = len(fileFolder[breedFolder]) // group
+
+        # Initialise empty numpy array
+        images = np.zeros((sampleSize, self.mImageWidth, self.mImageHeight, self.mImageChannels))
+
+        # Iterate through each group
+        for chunk in range(group):
+            for breedFolder in tqdm(listdir(self.mAnnPath)):
+                if chunk == group:
+                    files = fileFolder[breedFolder]
+                else:
+                    files = np.random.choice(fileFolder[breedFolder], groupSize[breedFolder], replace=False)
+                    fileFolder[breedFolder] = [x for x in fileFolder[breedFolder] if x not in files]
+                for file in files:
+                    # Create path to file
+                    path = Path(breedFolder, file)
+                    # Read the image and annotations tree
+                    img = self.readImage(self.mImPath / path.with_suffix(".jpg"))
+                    root = ET.parse(self.mAnnPath / path).getroot()
+                    # Get properties for the image
+                    size = root.find('size')
+                    height = int(size.find('height').text)
+                    width = int(size.find('width').text)
+                    # channels = size.find('depth')
+                    objects = root.findall('object')
+                    # Pictures may have multiple dogs in them
+                    # So we iterate through each dog in the picture and crop the image to the specific dog
+                    index = 0
+                    for object in objects:
+                        # Get the margins for the dog
+                        bndBox = object.find('bndbox')
+                        xMin = int(bndBox.find('xmin').text)
+                        xMax = int(bndBox.find('xmax').text)
+                        yMin = int(bndBox.find('ymin').text)
+                        yMax = int(bndBox.find('ymax').text)
+                        # Make the margins slightly bigger
+                        xMin = max(0, xMin - 4)
+                        xMax = min(width, xMax + 4)
+                        yMin = max(0, yMin - 4)
+                        yMax = min(height, yMax + 4)
+                        # Crop the image to focus on the specific dog
+                        # @todo: test whether adding a wider margin helps the discriminator more
+                        imgCropped = img[yMin:yMax, xMin:xMax, :]
+
+                        # Standardisation the images
+                        imgCropped = (imgCropped / 127.5) - 1
+
+                        # Get the interpolation method for scaling the picture
+                        if xMax - xMin > width:
+                            interpolation = tf.image.ResizeMethod.AREA  # Shrink
+                        else:
+                            interpolation = tf.image.ResizeMethod.BICUBIC  # Stretch
+
+                        # Resize image with pad to reserve aspect ratio
+                        # imgCropped = tf.image.resize_with_pad(imgCropped, self.mImageWidth, self.mImageHeight,
+                        #                                       method=interpolation, antialias=True)
+                        imgCropped = tf.image.resize(imgCropped, (self.mImageWidth, self.mImageHeight),
+                                                     method=interpolation, antialias=True)
+
+                        # Store image
+                        images[index] = imgCropped
+
+                        if index == sampleSize - 1:
+                            # Change type from float64 to float32 to save memory
+                            images = tf.cast(images, dtype=tf.float32)
+                            # Create dataset
+                            images = tf.data.Dataset.from_tensor_slices(images).shuffle(sampleSize).batch(
+                                self.mBatchSize, drop_remainder=True)
+                            yield images
+
+
 
     mImageWidth = None
     mImageHeight = None
@@ -134,11 +217,16 @@ class InputPipe():
 if __name__ == "__main__":
     io = InputPipe()
     # imga = io.readImage("Datasets\\images\\Images\\n02085620-Chihuahua\\n02085620_199.jpg")
-    io.loadAllImages(sampleSize=100)
-
-    for batch in io.mImages.take(1).as_numpy_iterator():
-        for pic in batch:
-            print(pic[32][32])
-            plt.imshow(pic, interpolation='nearest')
-            plt.show()
+    # io.loadAllImages()
+    for x in io.getNextImageChunk():
+        ...
+        # print(x)
+        # exit(-1)
+    # io.loadAllImages(sampleSize=100)
+    #
+    # for batch in io.mImages.take(1).as_numpy_iterator():
+    #     for pic in batch:
+    #         print(pic[32][32])
+    #         plt.imshow(pic, interpolation='nearest')
+    #         plt.show()
 
